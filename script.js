c1c8cc728fb9d22c5926a3b780c71cc3b6412e01
Wade Noble <wnobl374@mtroyal.ca>
2024-09-18 21:27:51 -0600

    test function initialized

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 import { GAMES } from "./games.js";
 
+function initBoard() {
+    let board = document.getElementById("game-board");
+    
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
+
+
 const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
