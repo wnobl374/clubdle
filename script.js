24e014a5c79ffd2295d5ef40134539759ec6a34b
Wade Noble <wnobl374@mtroyal.ca>
2024-09-18 21:25:19 -0600

    building test template

---
+++ script.js
@@ -0,0 +1,8 @@
+import { GAMES } from "./games.js";
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(rightGuessString)
