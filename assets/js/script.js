9f5a422f70b392c935e7fabe63739731ea5208b2
Wade Noble <wnobl374@mtroyal.ca>
2024-09-18 21:59:06 -0600

    Not sure why I put the method before the constructor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 import { GAMES } from "./games.js";
 
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+console.log(rightGuessString)
+
 function initBoard() {
     let board = document.getElementById("game-board");
     
@@ -17,12 +24,4 @@ function initBoard() {
     }
 }
 
-initBoard()
-
-
-const NUMBER_OF_GUESSES = 6;
-let guessesRemaining = NUMBER_OF_GUESSES;
-let currentGuess = [];
-let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
-console.log(rightGuessString)
+initBoard();
